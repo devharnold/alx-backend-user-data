f68eefba0618c6102d3030e02375c3f6101bda90
Henry Arnold <devharnold@gmail.com>
2024-04-12 12:21:58 +0300

    make correction

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,17 +5,11 @@ from typing import field
 import re
 
 
-def filter_datum(self, field: field, redaction: str, message: str, separator: str):
+def filter_datum(fields, redaction, message, separator):
     """Function returns log message eobfuscated
     Attributes:
         fields: a list of strings representing all fields to obfuscate
         redaction: a list representing by what the field will be obfuscated
         message: a string representing the log line
         separator: a string representing by which character is separating all fields"""
-    self.field = field
-    self.redaction = redaction
-    self.message = message
-    self.separator = separator
-    for i in field:
-        message = message.replace(i + separator, redaction + separator)
-    return message
+    return re.sub('|'.join(fields), redaction, message)
