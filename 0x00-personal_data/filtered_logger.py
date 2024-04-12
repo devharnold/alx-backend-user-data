261d053afb0674912b18418927057d268e0acd44
Henry Arnold <devharnold@gmail.com>
2024-04-12 12:17:19 +0300

    try one

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,17 +2,20 @@
 """log message obfuscate model.
 """
 from typing import field
+import re
 
 
-
-
-def filter_datum(field: field, redaction: str, message: str, separator: str):
+def filter_datum(self, field: field, redaction: str, message: str, separator: str):
     """Function returns log message eobfuscated
     Attributes:
         fields: a list of strings representing all fields to obfuscate
         redaction: a list representing by what the field will be obfuscated
         message: a string representing the log line
         separator: a string representing by which character is separating all fields"""
+    self.field = field
+    self.redaction = redaction
+    self.message = message
+    self.separator = separator
     for i in field:
         message = message.replace(i + separator, redaction + separator)
     return message
