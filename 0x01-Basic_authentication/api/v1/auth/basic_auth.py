42910f5861026f790a02f38f5645ad12452eff6f
Henry Arnold <devharnold@gmail.com>
2024-04-17 00:46:26 +0300

    add extract base function

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,3 +6,15 @@ from typing import TypeVar
 from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
+    """BasicAuthentication class"""
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """Function to handle and return the Base64 part of the Authorization header
+        for basic Authentication
+        """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith('Basic '):
+            return None
+        return authorization_header.split(' ')[1]
