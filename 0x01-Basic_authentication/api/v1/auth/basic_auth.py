09d8eec8d14b2554818e84e0d3770636d75c4ec5
Henry Arnold <devharnold@gmail.com>
2024-04-17 02:36:02 +0300

    last work

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -63,4 +63,28 @@ class BasicAuth(Auth):
         #if not isinstance(decoded_base64_authorization_header, str):
         #    return None
         #
-        
+    
+    def user_object_from_credentials(self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """
+        : returns the `user` instance based on his email and password
+        """
+        if type(user_email) == str and type(user_pwd) == str:
+            try:
+                users = User.search({"email": user_email})
+            except Exception:
+                return None
+            if len(users) <= 0:
+                return None
+            if users[0].is_valid_password(user_pwd):
+                return users[0]
+        return None
+    
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        : retrieves the user from a request
+        """
+        auth_header = self.authorization_header(request)
+        b64_auth_token = self.extract_base64_authorization_header(auth_header)
+        auth_token = self.decode_base64_authorization_header(b64_auth_token)
+        email, password = self.extract_user_credentials(auth_token)
+        return self.user_object_from_credentials(email, password)
