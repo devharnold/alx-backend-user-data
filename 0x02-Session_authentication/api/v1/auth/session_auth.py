481c41aef416eb72dcbc589343bb10678fd04bb3
Henry Arnold <devharnold@gmail.com>
2024-04-18 15:34:58 +0300

    change func definition

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -32,7 +32,7 @@ class SessionAuth(Auth):
         if type(session_id) is str:
             return self.user_id_by_session_id.get(session_id)
         
-    def current_user(self, request=None) -> TypeVar('User'):
+    def current_user(self, request=None):
         """
         : returns a user instance based on a cookie value
         """
