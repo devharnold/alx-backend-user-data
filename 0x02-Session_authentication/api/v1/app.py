694ea3c357b02a899a1dd50d73bc26c58f5761f6
Henry Arnold <devharnold@gmail.com>
2024-04-18 13:56:24 +0300

    try out

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -52,7 +52,7 @@ def authenticate_user():
             '/api/v1/status',
             '/api/v1/unauthorized',
             '/api/v1/forbidden',
-            '/api/v1/auth_session/login/'
+            '/api/v1/auth_session/login/',
         ]
         if auth.require_auth(request.path, excluded_paths):
             user = auth.current_user(request)
