7b7f85fdbcde37f324939caba19c2df762fc0f29
Henry Arnold <devharnold@gmail.com>
2024-04-19 05:03:21 +0300

    add simple files

--- 0x02-Session_authentication/api/v1/auth/basic_auth.py
+++ 0x02-Session_authentication/api/v1/auth/basic_auth.py
@@ -5,6 +5,7 @@ import re
 from typing import TypeVar, Tuple
 from api.v1.auth.auth import Auth
 import base64
+from models.user import User
 
 
 class BasicAuth(Auth):
--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,9 +4,8 @@
 import os
 from api.v1.auth.auth import Auth
 from uuid import uuid4
-from flask import jsonify
 from models.user import User
-
+from flask import request
 class SessionAuth(Auth):
     """SessionAuthenticate class
     """
@@ -38,3 +37,18 @@ class SessionAuth(Auth):
         """
         user_id = self.user_id_for_session_id(self.session_cookie(request))
         return User.get(user_id)
+    
+    def destroy_session(self, request=None):
+        """
+        : to destroy an authenticated session
+        Args:
+            request(object): the request object
+        Return: True if the session was successfuly destroyed
+        """
+        session_id = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_id)
+        if (request is None or session_id is None) or user_id is None:
+            return False
+        if session_id in self.user_id_by_session_id:
+            del self.user_id_by_session_id[session_id]
+        return True
