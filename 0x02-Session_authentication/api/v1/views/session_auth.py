2b3efed6ceec704a0f7b8fe4965f3f55adf92604
Henry Arnold <devharnold@gmail.com>
2024-04-19 10:55:32 +0300

    : add delete function

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -37,3 +37,14 @@ def login() -> Tuple[str, int]:
         return result
     return jsonify({"error": "wrong password"}), 401
     
+
+@app_views.route('/auth_session/logout', methods=['DELETE'], strict_slashes=False)
+def logout(self):
+    """
+    : deleting the session ID contains in the request as cookie
+    """
+    from api.v1.app import auth
+    is_deleted = auth.destroy_session(request)
+    if not is_deleted:
+        abort(404)
+    return jsonify({})
