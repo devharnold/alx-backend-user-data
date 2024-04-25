da202db999490432e7901a4d3707942f9cc6b349
Henry Arnold <devharnold@gmail.com>
2024-04-25 09:21:59 +0300

    retrieve user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -4,8 +4,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.session import session
 from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy import Optional
 
 from user import Base, User
 
@@ -22,7 +23,7 @@ class DB:
         self.__session = None
 
     @property
-    def _session(self) -> Session:
+    def _session(self) -> session:
         """Memoized session object
         """
         if self.__session is None:
@@ -37,8 +38,35 @@ class DB:
         Returns a new user object
         """
         new_user = User(email=email, hashed_password=hashed_password)
-        Session.add(new_user)
-        Session.commit()
+        session.add(new_user)
+        session.commit()
         return new_user
     
+    def find_user(user_id: int)-> Optional[User]:
+        """
+        Find user by their email address from the db
+        Parameters:
+            email(str): The user's email address
+        Returns:
+            User: The user object if found,or None if not found
+        """
+        return session.query(User).filter_by(email=user_id).first()
+    
+    def update_user(user_id: int, **kwargs) -> None:
+        """Updates a user by the user_id instance.
+        Parameters:
+            user_id(int): The unique identifier of the various users
+        Returns:
+            None"""
+        user = DB.find_user_by(user_id)
+        if user:
+            for key, value in kwargs.items():
+                if hasattr(user, key):
+                    setattr(user, key, value)
+                else:
+                    raise ValueError(f"Invalid attribute '{key}' for user.")
+            session.commit()
+        else:
+            print("User not found.")
+
     
